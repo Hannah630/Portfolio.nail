7b63430fcd149d696c8c7927e05ab84a9cf13ec6
Hannah <cc20010630@gmail.com>
2025-03-12 20:25:05 +0800

    初步上傳

---
+++ js/all.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    // ✅ 點擊 `.dropdown` 展開對應的 `.dropdown-open`
+    $(".dropdown").click(function(event) {
+        event.preventDefault(); // 防止跳轉
+  
+        let $dropdown = $(this); // 確保只影響被點擊的 `.dropdown`
+        let $dropdownMenu = $dropdown.next(".dropdown-open"); // 取得對應的 `.dropdown-open`
+  
+        $dropdown.toggleClass("active"); // 切換 `active` 類別
+        $dropdownMenu.slideToggle(); // 使用 `.slideToggle()` 來展開/收起
+    });
+  });
+  
